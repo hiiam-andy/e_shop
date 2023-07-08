693b6685452c8bb53b0d58cd7690c77ed7aa42f9
hiiam-andy <andrew.citb@gmail.com>
2023-07-08 18:10:27 +0300

    подключил фейкАПИ через redux, реализовал сайдбар

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,5 +1,46 @@
 import React from "react";
+import styles from "../../styles/Sidebar.module.css";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  const { list } = useSelector(({ categories }) => categories);
+
+  return (
+    <section className={styles.sidebar}>
+      <div className={styles.title}>Categories</div>
+      <nav>
+        <ul className={styles.menu}>
+          {list.map(({ id, name }) => {
+            return (
+              <li key={id}>
+                <NavLink
+                  className={({ isActive }) =>
+                    `${styles.link} ${isActive ? styles.active : ""}`
+                  }
+                  to={`/categories/${id}`}
+                >
+                  {name}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+
+      <div className={styles.footer}>
+        <a href="/help" className={styles.link}>
+          help
+        </a>
+
+        <a
+          href="/terms"
+          className={styles.link}
+          style={{ textDecoration: "underline" }}
+        >
+          Terms & Conditions
+        </a>
+      </div>
+    </section>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
+import App from "./components/App/App";
+
+import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import App from "./components/App/App";
-
 import "./styles/index.css";
+import { store } from "./features/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
